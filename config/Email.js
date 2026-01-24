c06eb36d3a55e5965aa8061bb7a845e337b5e833
susilkumar-dev <rajususil9@gmail.com>
2026-01-24 21:52:31 +0530

    24update

--- config/Email.js
+++ config/Email.js
@@ -2,19 +2,21 @@ const nodemailer = require("nodemailer");
 require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail', // ✅ Let Nodemailer handle the ports/security automatically
+    host: "smtp-relay.brevo.com", // ✅ Connecting to Brevo
+    port: 587,
+    secure: false, 
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
 });
 
-// Verify connection configuration
+// Verify connection
 transporter.verify(function (error, success) {
     if (error) {
         console.log("❌ Email Server Error: ", error);
     } else {
-        console.log("✅ Email Server is ready to send messages");
+        console.log("✅ Brevo Email Server is ready to send messages");
     }
 });
 
