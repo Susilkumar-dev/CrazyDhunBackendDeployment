28b1ec7a0e86fe46ff0738dd6fca28ea255096c7
susilkumar-dev <rajususil9@gmail.com>
2026-04-02 10:55:50 +0530

    last

--- config/Email.js
+++ config/Email.js
@@ -1,27 +1,28 @@
-const axios = require("axios");
+const nodemailer = require("nodemailer");
 
 const sendEmail = async ({ to, subject, html }) => {
   try {
-    const res = await axios.post(
-      "https://api.brevo.com/v3/smtp/email",
-      {
-        sender: { email: process.env.BREVO_USER, name: "Dhun Music" },
-        to: [{ email: to }],
-        subject,
-        htmlContent: html,
+    const transporter = nodemailer.createTransport({
+      host: "smtp-relay.brevo.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: "a6cbab001@smtp-brevo.com",
+        pass: process.env.BREVO_PASS, // SMTP key
       },
-      {
-        headers: {
-          "api-key": process.env.BREVO_PASS,
-          "Content-Type": "application/json",
-        },
-      }
-    );
+    });
 
-    console.log("✅ Email sent:", res.data);
+    const info = await transporter.sendMail({
+      from: `"Dhun Music" <${process.env.BREVO_USER}>`,
+      to,
+      subject,
+      html,
+    });
+
+    console.log("✅ Email sent:", info.messageId);
   } catch (error) {
-    console.error("❌ Email API Error:", error.response?.data || error.message);
-    throw error; // optional but useful
+    console.error("❌ Email Error:", error.message);
+    throw error;
   }
 };
 
